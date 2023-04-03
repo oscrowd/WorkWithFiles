bd0c979bd99c0605e172289d1a621e614f5187e5
oscrowd <oscrowd@gmail.com>
2023-04-03 22:11:00 +0300

    D-01.000.02_Task2

---
+++ Task2/Dir.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Dir
+    {
+        public string DirPath;
+        public Boolean DirExist()
+        {
+            var di = new DirectoryInfo(DirPath);
+            return di.Exists;
+        }
+
+        public long DirSize()
+        {
+            
+            var d = new DirectoryInfo(DirPath);
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize();
+            }
+            return size;
+        }
+    }  
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long DS = 0;
+            Boolean DirEx = false;
+            Dir dir = new Dir();
+            Console.WriteLine("Введите путь к директори:");
+            dir.DirPath = Console.ReadLine();
+            DirEx = dir.DirExist();
+
+            if (DirEx)
+            {
+                try
+                {
+                    DS = dir.DirSize();
+                    Console.WriteLine("Размер директори {0} - {1}", dir.DirPath, DS);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ошибка определения размера папки  {0} - {1}", dir.DirPath, e.Message);
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Папки {0} не существует", dir.DirPath);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
