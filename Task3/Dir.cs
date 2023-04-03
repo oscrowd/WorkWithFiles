29a59bf169b6c3e9e25969865ead80a790daa55b
oscrowd <oscrowd@gmail.com>
2023-04-03 23:34:38 +0300

    D-01.000.02_Task3

---
+++ Task3/Dir.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Dir
+    {
+        public string DirPath;
+
+        public object di()
+        {
+            var di = new DirectoryInfo(DirPath);
+            return di;
+        }
+        public Boolean DirExist()
+        {
+            var di = new DirectoryInfo(DirPath);
+            return di.Exists;
+        }
+
+        public long DirSize(string DPath)
+        {
+            
+            var d = new DirectoryInfo(DPath);
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                string DP = di.FullName;
+                size += DirSize(DP);
+            }
+            return size;
+        }
+        public void DelPathAndDir()
+        {
+            var di = new DirectoryInfo(DirPath);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+        }
+    }  
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long DS = 0;
+            long DSafterDel = 0;
+            long DelSize = 0;
+            Boolean DirEx = false;
+            Dir dir = new Dir();
+            Console.WriteLine("Введите путь к директори:");
+            dir.DirPath = Console.ReadLine();
+            DirEx = dir.DirExist();
+
+            if (DirEx)
+            {
+                try
+                {
+                    DS = dir.DirSize(dir.DirPath);
+                    Console.WriteLine("Исходный размер папки {0} - {1}", dir.DirPath, DS);
+                    dir.DelPathAndDir();
+                    DSafterDel = dir.DirSize(dir.DirPath);
+                    DelSize = DS - DSafterDel;
+                    Console.WriteLine("Освобождено - {0}", DelSize);
+                    Console.WriteLine("Текущий размер папки {0} - {1}", dir.DirPath, DSafterDel);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ошибка - {0}", e.Message);
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Папки {0} не существует", dir.DirPath);
+                Console.ReadKey();
+            }
+        }
+    }
+}
