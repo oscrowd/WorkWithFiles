2804c7cd2646dfa25f5586644e88dc0112efd94e
oscrowd <oscrowd@gmail.com>
2023-04-04 21:37:43 +0300

    D-01.000.04_Task4

--- Task4/BinaryData.cs
+++ Task4/BinaryData.cs
@@ -4,9 +4,13 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
 
-namespace FinalTask
+namespace Serialization
 {
+   
+
+
     public class BinaryData
     {
     
@@ -37,20 +41,19 @@ namespace FinalTask
                 using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
                 {
                     // Применяем специализированные методы Read для считывания соответствующего типа данных.
-                    //FloatValue = reader.ReadSingle();
-                     StringValue = reader.ReadString();
-                    //StringValue = reader.ReadByte();
-                    //IntValue = reader.ReadInt32();
-                    //BooleanValue = reader.ReadBoolean();
+                    FloatValue = reader.ReadSingle();
+                    StringValue = reader.ReadString();
+                    IntValue = reader.ReadInt32();
+                    BooleanValue = reader.ReadBoolean();
 
                 }
 
                 Console.WriteLine("Из файла считано:");
 
-                //Console.WriteLine("Дробь: " + FloatValue);
+                Console.WriteLine("Дробь: " + FloatValue);
                 Console.WriteLine("Строка: " + StringValue);
-                //Console.WriteLine("Целое: " + IntValue);
-                //Console.WriteLine("Булево значение " + BooleanValue);
+                Console.WriteLine("Целое: " + IntValue);
+                Console.WriteLine("Булево значение " + BooleanValue);
                 Console.ReadLine();
             }
         }
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinalTask
+
+namespace Serialization
 {
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Student(string name, string group, DateTime DoB)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = DoB;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            const string SettingsFileName = "D:\\test1\\Students.dat";
+            //const string SettingsFileName = "D:\\test1\\Students.dat";
             // Пишем
-            BinaryData binaryData = new BinaryData();
+            //BinaryData binaryData = new BinaryData();
             //binaryData.WriteValues(SettingsFileName);
-            binaryData.ReadValues(SettingsFileName);
-            
+            //binaryData.ReadValues(SettingsFileName);
+            // сохраняем путь к файлу (допустим, вы его скачали на рабочий стол)
+            string filePath = "D:\\test1\\Students.dat";
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                var newStudent = (Student)formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Имя: {newStudent.Name} --- Группа: {newStudent.Group}");
+            }
+            Console.ReadLine();
+
         }
+
+        
     }
 }
