8f3f6930cd090db114e530aec2af567edbaafbd1
oscrowd <oscrowd@gmail.com>
2023-04-04 20:49:40 +0300

    D-01.000.03_Task4

---
+++ Task4/BinaryData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    public class BinaryData
+    {
+    
+
+        public void WriteValues(string SettingsFileName)
+        {
+            // Создаем объект BinaryWriter и указываем, куда будет направлен поток данных
+            using (BinaryWriter writer = new BinaryWriter(File.Open(SettingsFileName, FileMode.Create)))
+            {
+                // записываем данные в разном формате
+                writer.Write(20.666F);
+                writer.Write(@"Тестовая строка");
+                writer.Write(55);
+                writer.Write(false);
+            }
+        }
+
+        public void ReadValues(string SettingsFileName)
+        {
+            float FloatValue;
+            string StringValue;
+            int IntValue;
+            bool BooleanValue;
+
+            if (File.Exists(SettingsFileName))
+            {
+                // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
+                using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+                {
+                    // Применяем специализированные методы Read для считывания соответствующего типа данных.
+                    //FloatValue = reader.ReadSingle();
+                     StringValue = reader.ReadString();
+                    //StringValue = reader.ReadByte();
+                    //IntValue = reader.ReadInt32();
+                    //BooleanValue = reader.ReadBoolean();
+
+                }
+
+                Console.WriteLine("Из файла считано:");
+
+                //Console.WriteLine("Дробь: " + FloatValue);
+                Console.WriteLine("Строка: " + StringValue);
+                //Console.WriteLine("Целое: " + IntValue);
+                //Console.WriteLine("Булево значение " + BooleanValue);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SettingsFileName = "D:\\test1\\Students.dat";
+            // Пишем
+            BinaryData binaryData = new BinaryData();
+            //binaryData.WriteValues(SettingsFileName);
+            binaryData.ReadValues(SettingsFileName);
+            
+        }
+    }
+}
