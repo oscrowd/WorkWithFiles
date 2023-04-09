4a97258e8095128cbc312c8d2a08c56c2a59cea6
oscrowd <oscrowd@gmail.com>
2023-04-09 15:36:28 +0300

    D-01.000.05_Task4

--- Task4/BinaryData.cs
+++ Task4/BinaryData.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization.Formatters.Binary;
 
-namespace Serialization
+namespace FinalTask
 {
    
 
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -8,39 +8,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Serialization
+namespace FinalTask
 {
     [Serializable]
-    class Student
+    public class Student
     {
         public string Name { get; set; }
         public string Group { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public Student(string name, string group, DateTime DoB)
-        //public Student(string name, string group)
-        {
-            Name = name;
-            Group = group;
-            DateOfBirth = DoB;
-        }
+       
     }
     internal class Program
     {
         static void Main(string[] args)
         {
             const string SettingsFileName = "D:\\test1\\Students.dat";
-            BinaryData binaryData = new BinaryData();
+            //BinaryData binaryData = new BinaryData();
             //binaryData.WriteValues(SettingsFileName);
             //binaryData.ReadValues(SettingsFileName);
              
 
             string filePath = "D:\\test1\\Students.dat";
             BinaryFormatter formatter = new BinaryFormatter();
+            //Student studentall = new Student();
             using (var fs = new FileStream(filePath, FileMode.Open))
             {
-                var newStudent = (Student)formatter.Deserialize(fs);
-                Console.WriteLine("Объект десериализован");
-                Console.WriteLine($"Имя: {newStudent.Name} --- Группа: {newStudent.Group}");
+                Student[] students = (Student[])formatter.Deserialize(fs);
+                foreach (Student student in students)
+                {
+                    Console.WriteLine(student.Name);
+                }
+                
             }
             Console.ReadLine();
 
